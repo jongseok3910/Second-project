5009802900a6141f2505f1c1dd74034a4333517f
hjenyo7673 <hjenyo7673@gmail.com>
2021-03-17 17:08:22 +0900

    ..

--- src/com/itwill/shop/dao/CardDaoImpl.java
+++ src/com/itwill/shop/dao/CardDaoImpl.java
@@ -14,57 +14,5 @@ import com.itwill.shop.domain.Card;
 
 public class CardDaoImpl implements CardDao {
 	
-	private SqlSessionFactory sqlSessionFactory;
-	public CardDaoImpl() throws Exception{
-		try {
-			InputStream mybatisConfigInputStream=
-					Resources.getResourceAsStream("mybatis-config.xml");
-			SqlSessionFactoryBuilder sqlSessionFactoryBuilder=new SqlSessionFactoryBuilder();
-		this.sqlSessionFactory=
-				sqlSessionFactoryBuilder.build(mybatisConfigInputStream);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
 	
-
-	@Override
-	public Card findCardByNo(Integer CardNo) {
-		SqlSession sqlsession = sqlSessionFactory.openSession(true);
-		CartMapper cardMapper = sqlsession.getMapper(CartMapper.class);
-		Card card = cardMapper.findCardByNo(CardNo);
-		return card;
-	}
-
-	@Override
-	public List<Card> findCardAll() {
-		SqlSession sqlSession = sqlSessionFactory.openSession(true);
-		CartMapper cardMapper = sqlSession.getMapper(CartMapper.class);
-		List<Card> cardList = cardMapper.findCardAll();
-		return cardList;
-	}
-
-	@Override
-	public int insertCard(Card Card) {
-		SqlSession sqlSession = sqlSessionFactory.openSession(true);
-		CartMapper cardMapper = sqlSession.getMapper(CartMapper.class);
-		int insertRow = cardMapper.insertCard(Card);
-		return insertRow;
-	}
-
-	@Override
-	public int updateCardByNo(Card Card) {
-		SqlSession sqlSession = sqlSessionFactory.openSession(true);
-		CartMapper cardMapper = sqlSession.getMapper(CartMapper.class);
-		int updateRow = cardMapper.updateCardByNo(Card);
-		return updateRow;
-	}
-
-	@Override
-	public int deleteCardByNo(Integer CardNo) {
-		SqlSession sqlSession = sqlSessionFactory.openSession(true);
-		CartMapper cardMapper = sqlSession.getMapper(CartMapper.class);
-		int deleteRow = cardMapper.deleteCardByNo(CardNo);
-		return deleteRow;
-	}
 }
