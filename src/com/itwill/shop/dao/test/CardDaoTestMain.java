969d52e7c3f01e345287d4069425cabd505cedf0
BJH12400 <bjh12400@gmail.com>
2021-03-17 22:14:16 +0900

    error

--- src/com/itwill/shop/dao/test/CardDaoTestMain.java
+++ src/com/itwill/shop/dao/test/CardDaoTestMain.java
@@ -12,11 +12,11 @@ public class CardDaoTestMain {
 		
 		//System.out.println(cardDao.findCardAll()); //  =  성공 
 		
-		Card card= new Card(2,"5555555555555555", "농협카드", 0, 2);
-		//System.out.println(cardDao.insertCard(card)); //  = 실패 
+		Card card= new Card(0,"5555555555555555", "농협카드", 0, 2);
+		System.out.println(cardDao.insertCard(card)); 
 		
 		Card card1= new Card("111111111111", "국민카드", 1, 1);
-		System.out.println(cardDao.updateCardByNo(card1));  //= 실패
+		//System.out.println(cardDao.updateCardByNo(card1));  //= 실패
 		//System.out.println(cardDao.deleteCardByNo(2)); // =  성공 
 		
 		
