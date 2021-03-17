b8cc268e2d81130a00ce5f66ae954265e3163c82
hjenyo7673 <hjenyo7673@gmail.com>
2021-03-17 16:09:16 +0900

    ..

--- src/com/itwill/shop/dao/CardDao.java
+++ src/com/itwill/shop/dao/CardDao.java
@@ -6,7 +6,7 @@ import com.itwill.shop.domain.Address;
 import com.itwill.shop.domain.Card;
 
 public interface CardDao {
-	Address findCardByNo(Integer cardNo);
+	Card findCardByNo(Integer cardNo);
 	public List<Card> findcardAll();
 	public int insertCard(Card card);
 	public int updateCardByNo(Card card);
