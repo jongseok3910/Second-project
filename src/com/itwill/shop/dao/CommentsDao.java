dbe76fc04bddaaa692c710ad25e22230d8557905
BJH12400 <bjh12400@gmail.com>
2021-03-19 14:36:30 +0900

    comments

--- src/com/itwill/shop/dao/CommentsDao.java
+++ src/com/itwill/shop/dao/CommentsDao.java
@@ -7,7 +7,7 @@ import com.itwill.shop.domain.Comments;
 public interface CommentsDao {
 	
 	Comments findCommentByno(int commentsNo);
-	List<Comments> findCommentAll();
+	List<Comments> findCommentAll(int food_no);
 	int insertComments(Comments comments);
 	int updateCommentsByNo(Comments comments);
 	int deleteCommentsByNo(int commentsNo);
