5c1c0f9898834234fba11a76772f09178482b1cb
chrisdohyung <chris.dohyung@gmail.com>
2021-03-19 18:19:42 +0900

    ..

--- src/com/itwill/shop/dao/mapper/MembersMapper.java
+++ src/com/itwill/shop/dao/mapper/MembersMapper.java
@@ -7,13 +7,14 @@ import com.itwill.shop.domain.Members;
 
 public interface MembersMapper {
 	/*
-	 * 인터페이스의 풀네임은 StudentMapper.xml의 namespace와일치
-	 * 메쏘드이름은 	StudentMapper.xml 파일의 id와일치
-	 * 메쏘드인자타입은 StudentMapper.xml 파일의 parameterType 와일치
-	 * 메쏘드리턴타입은 StudentMapper.xml 파일의 resultType 와일치(ResultSet이 한 개 이상일 경우 반환타입이 List이다)
+	 * 인터페이스의 풀네임은 MembersMapper.xml의 namespace와일치
+	 * 메쏘드이름은 	MembersMapper.xml 파일의 id와일치
+	 * 메쏘드인자타입은 MembersMapper.xml 파일의 parameterType 와일치
+	 * 메쏘드리턴타입은 MembersMapper.xml 파일의 resultType 와일치(ResultSet이 한 개 이상일 경우 반환타입이 List이다)
 	 */
 
 	public List<Members> selectAll() throws Exception;
+	public ArrayList<Members> findMembersAll() throws Exception;
 	public Members findMembersByNo(int members_no) throws Exception;
 	public Members findMembersById(String members_email) throws Exception;
 	public Members findMembersByPassWord(String members_password) throws Exception;
